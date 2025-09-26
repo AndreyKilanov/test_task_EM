43da8c380488da90f2b36d13706a5b7dae0fcc43
AndreyKilanov <8941519@mail.ru>
2025-09-26 19:57:35 +0500

    creating a basic architecture

---
+++ backend/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Spectacular API schema generation
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+]
