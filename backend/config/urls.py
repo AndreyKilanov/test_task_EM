e0527aaa623e24854a37fa8184315f22d52040b8
AndreyKilanov <8941519@mail.ru>
2025-10-02 18:49:37 +0500

    add users urls and resource urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
@@ -9,4 +11,13 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
+    path("api/users/", include("users.urls"), name="users"),
+
+    path('api/v1/', include('resources.urls')),
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
