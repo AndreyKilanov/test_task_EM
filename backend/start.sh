ef32fd9000a9e26776b0298ed357424c00476349
AndreyKilanov <8941519@mail.ru>
2025-10-02 18:55:53 +0500

    fix

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ "$DEBUG" = "False" ]; then
+    echo "Waiting for PostgreSQL..."
+
+    until pg_isready -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME"; do
+      echo "PostgreSQL is unavailable - sleeping"
+      sleep 1
+    done
+
+    echo "PostgreSQL is ready!"
+fi
+
 echo "Making migrations..."
 python manage.py makemigrations
 
@@ -10,5 +21,15 @@ python manage.py migrate --noinput
 echo "Creating superuser..."
 python manage.py create_su
 
+echo "Creating access control..."
+python manage.py init_access_control
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Starting server..."
+if [ "$DEBUG" = "True" ]; then
+    python manage.py runserver 0.0.0.0:8000
+else
+    exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+fi
